9741bd04aed68474a08d2e88e9d6cc5c4ea904ea
Bohdan Mrychko <mrychko.b@gmail.com>
2021-03-01 18:33:41 +0200

    project init

---
+++ react-kpi-project/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Loggin from "./Components/Loggin/Loggin"
+
+function App() {
+  return (
+    <div className="App">
+      <Loggin></Loggin>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-kpi-project/src/Components/Loggin/Loggin.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Loggin.module.scss';
+
+const Loggin = () => {
+  
+  return (
+  <div className={styles.Loggin}>
+    Loggin Component
+  </div>
+)
+};
+
+Loggin.propTypes = {};
+
+Loggin.defaultProps = {};
+
+export default Loggin;
