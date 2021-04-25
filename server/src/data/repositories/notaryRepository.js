e7b64f343be940240f6c3dc451b9b3bddf8229f1
Roman Marianenko <kpimarianenko@gmail.com>
2021-04-25 15:46:21 +0300

    backend logic end

--- server/src/data/repositories/notaryRepository.js
+++ server/src/data/repositories/notaryRepository.js
@@ -1,3 +1,4 @@
+import Sequelize from 'sequelize';
 import {
   NotaryModel,
   ContactsModel,
@@ -10,6 +11,8 @@ import {
 } from '../models/index';
 import BaseRepository from './baseRepository';
 
+const { Op } = Sequelize;
+
 class NotaryRepository extends BaseRepository {
   getById(id) {
     return this.model.findOne({
@@ -19,6 +22,110 @@ class NotaryRepository extends BaseRepository {
       },
       include: [{
         model: ContactsModel,
+        attributes: {
+          exclude: ['areaId', 'regionId', 'localityId']
+        },
+        include: [RegionModel, AreaModel, LocalityModel, PhoneNumberModel]
+      }, {
+        model: EmploymentModel,
+        attributes: {
+          exclude: ['organizationId']
+        },
+        include: [{
+          model: OrganizationModel,
+          attributes: {
+            exclude: ['contactId']
+          },
+          include: [{
+            model: ContactsModel,
+            attributes: {
+              exclude: ['areaId', 'regionId', 'localityId']
+            },
+            include: [RegionModel, AreaModel, LocalityModel, PhoneNumberModel]
+          }]
+        }]
+      }]
+    });
+  }
+
+  getNotariesByName(firstName, middleName, lastName, certificateNumber) {
+    return this.model.findAll({
+      where: {
+        [Op.and]: [{
+          ...(firstName && {
+            firstName: {
+              [Op.iLike]: `%${firstName}%`
+            }
+          }),
+          ...(middleName && {
+            middleName: {
+              [Op.iLike]: `%${middleName}%`
+            }
+          }),
+          ...(lastName && {
+            lastName: {
+              [Op.iLike]: `%${lastName}%`
+            }
+          }),
+          ...(certificateNumber && {
+            certificateNumber: {
+              [Op.iLike]: `%${certificateNumber}%`
+            }
+          })
+        }]
+      },
+      attributes: {
+        exclude: ['contactId', 'employmentId']
+      },
+      include: [{
+        model: ContactsModel,
+        attributes: {
+          exclude: ['areaId', 'regionId', 'localityId']
+        },
+        include: [RegionModel, AreaModel, LocalityModel, PhoneNumberModel]
+      }, {
+        model: EmploymentModel,
+        attributes: {
+          exclude: ['organizationId']
+        },
+        include: [{
+          model: OrganizationModel,
+          attributes: {
+            exclude: ['contactId']
+          },
+          include: [{
+            model: ContactsModel,
+            attributes: {
+              exclude: ['areaId', 'regionId', 'localityId']
+            },
+            include: [RegionModel, AreaModel, LocalityModel, PhoneNumberModel]
+          }]
+        }]
+      }]
+    });
+  }
+
+  getNotariesByAddress(regionId, areaId, localityId, address = '') {
+    return this.model.findAll({
+      attributes: {
+        exclude: ['contactId', 'employmentId']
+      },
+      include: [{
+        model: ContactsModel,
+        where: {
+          ...(regionId && {
+            regionId
+          }),
+          ...(areaId && {
+            areaId
+          }),
+          ...(localityId && {
+            localityId
+          }),
+          address: {
+            [Op.iLike]: `%${address}%`
+          }
+        },
         attributes: {
           exclude: ['areaId', 'regionId', 'localityId']
         },
--- server/src/data/repositories/organizationRepository.js
+++ server/src/data/repositories/organizationRepository.js
@@ -1,5 +1,8 @@
+import Sequelize from 'sequelize';
 import { OrganizationModel, ContactsModel, AreaModel, RegionModel, LocalityModel, PhoneNumberModel } from '../models';
 import BaseRepository from './baseRepository';
+
+const { Op } = Sequelize;
 
 class OrganizationRepository extends BaseRepository {
   getOrganizationById(id) {
@@ -10,6 +13,55 @@ class OrganizationRepository extends BaseRepository {
       },
       include: [{
         model: ContactsModel,
+        attributes: {
+          exclude: ['areaId', 'regionId', 'localityId']
+        },
+        include: [RegionModel, AreaModel, LocalityModel, PhoneNumberModel]
+      }]
+    });
+  }
+
+  getOrganizationsByName(name = '') {
+    return this.model.findAll({
+      where: {
+        name: {
+          [Op.iLike]: `%${name}%`
+        }
+      },
+      attributes: {
+        exclude: ['contactId']
+      },
+      include: [{
+        model: ContactsModel,
+        attributes: {
+          exclude: ['areaId', 'regionId', 'localityId']
+        },
+        include: [RegionModel, AreaModel, LocalityModel, PhoneNumberModel]
+      }]
+    });
+  }
+
+  getOrganizationsByAddress(regionId, areaId, localityId, address = '') {
+    return this.model.findAll({
+      attributes: {
+        exclude: ['contactId']
+      },
+      include: [{
+        model: ContactsModel,
+        where: {
+          ...(regionId && {
+            regionId
+          }),
+          ...(areaId && {
+            areaId
+          }),
+          ...(localityId && {
+            localityId
+          }),
+          address: {
+            [Op.iLike]: `%${address}%`
+          }
+        },
         attributes: {
           exclude: ['areaId', 'regionId', 'localityId']
         },
